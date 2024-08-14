6cb38779b5deb87c5e6cf5fb0c3c2ee4928bc6bc
Juan Gonon <juanfegonon27@gmail.com>
2024-08-14 10:35:50 -0600

    middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { usersSlice } from "./users/slice"
 
+const persistenceLocalStorageMiddleware = (store) => (next) => (action) => {
+    console.log(store.getState())
+    console.log(action)
+    next(action)
+    console.log(store.getState())
+}
+
 export const store = configureStore({
     reducer: {
         users: usersSlice.reducer
     },
+   middleware(getDefaultMiddleware) {
+       return getDefaultMiddleware().concat(persistenceLocalStorageMiddleware)
+   },
 })
 
 export type RootState = ReturnType<typeof store.getState>
