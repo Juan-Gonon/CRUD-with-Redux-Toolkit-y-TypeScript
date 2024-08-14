a22c62fdffcc5e5c03d697a9e0f5e86cca628b30
Juan Gonon <juanfegonon27@gmail.com>
2024-08-14 15:37:42 -0600

    manejo de errores form

--- src/components/CreateNewUser.tsx
+++ src/components/CreateNewUser.tsx
@@ -1,12 +1,14 @@
-import { Button, Card, TextInput, Title } from "@tremor/react"
-import React from "react"
+import { Button, Card, TextInput, Title, Badge } from "@tremor/react"
+import { useState } from "react"
 import { useUserActions } from "../hook/useUserActions"
 
 export function CreateNewUser() {
 
     const { addUser } = useUserActions()
+    const [result, setResult] = useState<'ok' | 'ko' | null >(null)
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        setResult(null)
         e.preventDefault() 
         const form = e.currentTarget
         const formData = new FormData(form)
@@ -14,7 +16,13 @@ export function CreateNewUser() {
         const email = formData.get('email') as string
         const github = formData.get('github') as string
 
+        if(!name || !email || !github ){
+            return setResult('ko')
+        }
+
         addUser({ name, email, github})
+        setResult('ok')
+        form.reset()
     }
     
   return (
@@ -29,6 +37,10 @@ export function CreateNewUser() {
                 <Button  type="submit"  >
                     Crear usuario
                 </Button>
+                <span>
+                    { result === 'ok' && <Badge color='green'> Guardado correctamente </Badge>}
+                    { result === 'ko' && <Badge color='red'> Error con los campos </Badge> }
+                </span>
             </div>
         </form>
     </Card>
